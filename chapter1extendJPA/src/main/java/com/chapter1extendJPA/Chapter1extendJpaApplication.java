f42700f66c66648a4e3937e6a5f380e0416fa420
TaeheeWoo94 <wth0086@gmail.com>
2023-10-16 20:03:26 +0900

    first commit

---
+++ chapter1extendJPA/src/main/java/com/chapter1extendJPA/Chapter1extendJpaApplication.java
@@ -0,0 +1,13 @@
+package com.chapter1extendJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chapter1extendJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chapter1extendJpaApplication.class, args);
+	}
+
+}
